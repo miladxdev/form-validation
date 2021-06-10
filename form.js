68dcf8e854043e4c5f79e580460746d128ed23a4
Milad <miladxgh@outlook.com>
2021-06-10 23:23:10 +0430

    signup page works

--- form.js
+++ form.js
@@ -1,2 +1,13 @@
 let signupBtn = document.getElementById('signup');
 let signupElement = document.getElementById('signup-container');
+let backBtn = document.getElementById('back-btn');
+
+signupBtn.addEventListener('click', () => {
+    signupElement.style.left = '0';
+    // signupElement.style.opacity = '1';
+});
+
+backBtn.addEventListener('click', () => {  
+    signupElement.style.left = '100%';
+    // signupElement.style.opacity = '0';
+});
